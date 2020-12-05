d5e7db0a071459b03cc462b8d588f776a713cf68
Nestorsoiza <nestor.soiza83@gmail.com>
2020-12-05 12:38:47 +0100

    FINAL
    SIN COMPILAR

--- src/briscazo/Ejercicio_BRISCA.java
+++ src/briscazo/Ejercicio_BRISCA.java
@@ -4,14 +4,14 @@ public class Ejercicio_BRISCA {
 
 	public static void main(String[] args) {
 		Baraja b = new Baraja();
-//		System.out.println("Hay " + b.cartasDisponible() + " cartas disponibles");
-//		b.siguienteCarta();
-//		b.darCartas(5);
-//		
-//		System.out.println("Hay " + b.cartasDisponible() + " cartas disponibles");
-//		System.out.println("Cartas sacadas de momento");
-//		b.cartasMonton();
-//		b.barajar();
+		System.out.println("Hay " + b.cartasDisponible() + " cartas disponibles");
+		b.siguienteCarta();
+		b.darCartas(5);
+		
+		System.out.println("Hay " + b.cartasDisponible() + " cartas disponibles");
+		System.out.println("Cartas sacadas de momento");
+		b.cartasMonton();
+		b.barajar();
 		Carta[] cartas = b.darCartas(5);
 		
 		System.out.println("Cartas sacadas despues de barajar ");
